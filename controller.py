aec69cbf6664a42942db6e49e8b3a64107b01215
Aurélien Gâteau <agateau@kde.org>
2009-05-29 15:38:25 +0200

    Use QMainWindow

--- controller.py
+++ controller.py
@@ -1,39 +1,44 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-from toolbar import ToolBar
+from ui_mainwindow import Ui_MainWindow
 from bubble import Bubble
 
 class Controller(QObject):
     def __init__(self):
         QObject.__init__(self)
+
+        self.window = QMainWindow()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self.window)
+
         self.scene = QGraphicsScene()
-        self.view = QGraphicsView(self.scene)
-        self.view.setRenderHints(QPainter.Antialiasing | QPainter.SmoothPixmapTransform)
+        self.ui.view.setScene(self.scene)
+
         self.pixmapItem = QGraphicsPixmapItem()
         self.pixmapItem.setZValue(-1)
         self.scene.addItem(self.pixmapItem)
 
-        self.shapeToolBar = ToolBar(Qt.Vertical)
-        self.shapeToolBar.setZValue(1)
-        self.shapeToolBar.addTool("bubble", self.addBubble)
-        self.scene.addItem(self.shapeToolBar)
-        self.shapeToolBar.setPos(0, 0)
+        QObject.connect(self.ui.actionBubble, SIGNAL("triggered()"), \
+            self.addBubble)
+
+        self.window.resize(700, 500)
 
 
     def show(self):
-        self.view.show()
+        self.window.show()
 
 
     def load(self, fileName):
         pix = QPixmap(fileName)
         self.pixmapItem.setPixmap(pix)
-        self.view.resize(pix.size())
+        self.scene.setSceneRect(QRectF(pix.rect()))
 
 
     def addBubble(self):
         bubble = Bubble("A Bubble")
-        bubble.setPos(self.view.width() / 2, self.view.height() / 2)
+        rect = self.pixmapItem.boundingRect()
+        bubble.setPos(rect.width() / 2, rect.height() / 2)
         self.scene.addItem(bubble)
 
 
