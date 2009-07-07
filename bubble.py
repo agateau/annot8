1555a5cc23c6cd2c2b40e6cfce27dd81d9359389
Aurélien Gâteau <agateau@kde.org>
2009-07-08 00:32:49 +0200

    Bubble anchor size can now be adjusted

--- bubble.py
+++ bubble.py
@@ -1,11 +1,18 @@
+import math
+
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+
+from handle import Handle
 
 OPACITY = 0.8
 MARGIN = 10
 
-ANCHOR_WIDTH = 20
-ANCHOR_HEIGHT = 20
+ANCHOR_THICKNESS = 20
+
+def computeAnchorDelta(vector, length):
+    angle = math.atan(vector.y() / vector.x()) + math.pi / 2
+    return QPointF(length * math.cos(angle), length * math.sin(angle))
 
 class Bubble(QGraphicsPathItem):
     def __init__(self):
@@ -15,6 +22,11 @@ class Bubble(QGraphicsPathItem):
 
         self.setBrush(QColor.fromHsvF(0, 0, 1., OPACITY))
 
+        self.anchorHandle = Handle(self, 0, 0)
+        self.bubbleHandle = Handle(self, ANCHOR_THICKNESS, -ANCHOR_THICKNESS)
+        self.anchorHandle.addLinkedItem(self)
+        self.bubbleHandle.addLinkedItem(self)
+
         self.text = QGraphicsTextItem(self)
         self.text.setTextInteractionFlags(Qt.TextEditorInteraction)
         QObject.connect(self.text.document(), SIGNAL("contentsChanged()"), \
@@ -22,34 +34,47 @@ class Bubble(QGraphicsPathItem):
         self.text.setPlainText("")
 
     def adjustSizeFromText(self):
-        # Place anchor
-        anchor = QPolygonF([QPointF(0, 0), QPointF(0, -ANCHOR_HEIGHT), QPointF(ANCHOR_WIDTH, -ANCHOR_HEIGHT)])
-
-        # Place bubble rect above anchor
+        # Place bubble rect above bubbleHandle
         self.text.adjustSize()
-        rect = QRectF(QPointF(0, 0), self.text.document().size())
+        rectSize = self.text.document().size()
+        rect = QRectF(
+            self.bubbleHandle.x(), self.bubbleHandle.y() - rectSize.height(),
+            rectSize.width(), rectSize.height()
+            )
         rect.adjust(-MARGIN, 0, MARGIN, 2*MARGIN)
 
-        minWidth = 2*MARGIN + ANCHOR_WIDTH
+        minWidth = 2*MARGIN + ANCHOR_THICKNESS
         if rect.width() < minWidth:
             rect.setWidth(minWidth)
-        rect.translate(0, -rect.height() - ANCHOR_HEIGHT)
 
         # Position text in bubble rect
         self.text.setPos(rect.left() + MARGIN, rect.top() + MARGIN)
+
+        # Compute anchor polygon
+        center = rect.center()
+        delta = computeAnchorDelta(center - self.anchorHandle.pos(), ANCHOR_THICKNESS / 2)
+
+        anchor = QPolygonF([
+            self.anchorHandle.pos(),
+            center + delta,
+            center - delta])
 
         # Avoid blurry borders
         anchor.translate(0.5, 0.5)
         rect.translate(0.5, 0.5)
 
+        polygon = anchor.united(QPolygonF(rect))
         path = QPainterPath()
-        path.addRect(rect)
-        path.addPolygon(anchor)
-        self.setPath(path.simplified())
+        path.addPolygon(polygon)
+        self.setPath(path)
 
 
     def itemChange(self, change, value):
         if change == QGraphicsItem.ItemSceneHasChanged:
             self.text.setFocus()
         return QGraphicsPathItem.itemChange(self, change, value)
+
+
+    def handleMoved(self, handle):
+        self.adjustSizeFromText()
 # vi: ts=4 sw=4 et
