6762dbf96c3c14ab9a18855d0884364566299b4d
Aurélien Gâteau <agateau@kde.org>
2009-07-07 18:52:56 +0200

    Nicer way to link a line and its handles

--- handle.py
+++ handle.py
@@ -6,7 +6,19 @@ HANDLE_RADIUS = 5
 class Handle(QGraphicsEllipseItem):
     def __init__(self, parent, x, y):
         QGraphicsEllipseItem.__init__(self, parent)
+        self.linkedItems = []
         self.setRect(-HANDLE_RADIUS, -HANDLE_RADIUS, 2 * HANDLE_RADIUS, 2 * HANDLE_RADIUS)
         self.setFlag(QGraphicsItem.ItemIsMovable)
         self.setPos(x, y)
         self.setBrush(QColor.fromHsvF(0, 0, 1., 0.8))
+
+    def addLinkedItem(self, item):
+        self.linkedItems.append(item)
+
+    def itemChange(self, change, value):
+        if change == QGraphicsItem.ItemPositionHasChanged:
+            for item in self.linkedItems:
+                item.handleMoved(self)
+        return QGraphicsEllipseItem.itemChange(self, change, value)
+
+# vi: ts=4 sw=4 et
